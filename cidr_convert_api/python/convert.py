d0b8993ee32671e16e800a1d33166b505999e0fc
albertorreyes2 <44677226+albertorreyes2@users.noreply.github.com>
2022-03-16 16:04:51 -0600

    feat: changes in def cidr_to_mask

--- cidr_convert_api/python/convert.py
+++ cidr_convert_api/python/convert.py
@@ -2,10 +2,16 @@
 class CidrMaskConvert:
 
     def cidr_to_mask(self, val):
-        return val
+        print(self, val)
+        network, net_bits = val.split('/')
+        host_bits = 32 - int(net_bits)
+        val = socket.inet_ntoa(struct.pack('!I', (1 << 32) - (1 << host_bits)))
+        return str(val)
 
     def mask_to_cidr(self, val):
         return val
+
+
 
 class IpValidate:
 
